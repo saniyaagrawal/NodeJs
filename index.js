b748f452c3f4ccc8ce71cce87851c2edc1ad060e
saniyaagrawal <saniya.agrawal30@gmail.com>
2020-08-22 22:46:23 +0530

    assignment 1

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const http = require('http');
 const bodyParser= require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promotionRouter = require('./routes/promotionRoute');
+const leaderRouter = require('./routes/leaderRoute');
 
 const hostname='localhost';
 const port=3000;
@@ -12,27 +14,12 @@ const app=express();
 
 app.use(morgan('dev'));
 
-
 app.use('/dishes', dishRouter);
+app.use('/promotions', promotionRouter);
+app.use('/leaders', leaderRouter);
+
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
-// app.get('/dishes/:dishId', (req,res,next)=> {
-//     res.end('get dish'+ req.params.dishId);
-// });
-
-// app.put('/dishes/:dishId', (req,res,next)=> {
-//     res.write('put dish'+ req.params.dishId);
-//     res.end('with details'+req.body.name+ 'and' +req.body.description)
-// });
-
-// app.post('/dishes/:dishId', (req,res,next)=> {
-//     res.statusCode=403;
-//     res.end('Not supported post');
-// });
-
-// app.delete('/dishes/:dishId', (req,res,next)=> {
-//     res.end('delete');
-// });
 
 app.use((req,res,next)=> {
     res.statusCode=200;
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -24,4 +24,20 @@ dishRouter.route('/')
     res.end('delete all dishes');
 });
 
+dishRouter.route('/:dishId')
+.get((req,res,next)=> {
+    res.end('get dish'+ req.params.dishId);
+})
+.put((req,res,next)=> {
+    res.write('put dish'+ req.params.dishId);
+    res.end('with details'+req.body.name+ 'and' +req.body.description)
+})
+.post((req,res,next)=> {
+    res.statusCode=403;
+    res.end('Not supported post');
+})
+.delete((req,res,next)=> {
+    res.end('delete');
+});
+
 module.exports=dishRouter;
