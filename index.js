f4aa6d8eaed7ee8f56b85347dd43becbcc4682a4
saniyaagrawal <saniya.agrawal30@gmail.com>
2020-08-22 21:55:44 +0530

    express simple restapi

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const morgan=require('morgan');
 const http = require('http');
+const bodyParser= require('body-parser');
 
 const hostname='localhost';
 const port=3000;
@@ -9,6 +10,48 @@ const app=express();
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (req,res,next)=> {
+    res.statusCode=200;
+    res.setHeader('Content-Type','text/plain');
+    next();
+});
+
+app.get('/dishes', (req,res,next)=> {
+    res.end('get all dishes');
+});
+
+app.put('/dishes', (req,res,next)=> {
+    res.statusCode=403;
+    res.end('put not supported: ');
+});
+
+app.post('/dishes', (req,res,next)=> {
+    res.end('posting: '+req.body.name+ 'with' +req.body.description);
+});
+
+app.delete('/dishes', (req,res,next)=> {
+    res.end('delete all dishes');
+});
+
+app.get('/dishes/:dishId', (req,res,next)=> {
+    res.end('get dish'+ req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req,res,next)=> {
+    res.write('put dish'+ req.params.dishId);
+    res.end('with details'+req.body.name+ 'and' +req.body.description)
+});
+
+app.post('/dishes/:dishId', (req,res,next)=> {
+    res.statusCode=403;
+    res.end('Not supported post');
+});
+
+app.delete('/dishes/:dishId', (req,res,next)=> {
+    res.end('delete');
+});
 
 app.use((req,res,next)=> {
     res.statusCode=200;
